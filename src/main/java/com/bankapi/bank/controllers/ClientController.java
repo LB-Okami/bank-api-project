c64d71edab9234302a88addf0df30c18569dc945
LB-Okami <lucabtei@outlook.com>
2023-08-17 17:03:02 -0300

    Client base Methods

--- src/main/java/com/bankapi/bank/controllers/ClientController.java
+++ src/main/java/com/bankapi/bank/controllers/ClientController.java
@@ -1,7 +1,6 @@
 package com.bankapi.bank.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bankapi.bank.model.Client;
 import com.bankapi.bank.services.ClientService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -33,10 +33,28 @@ public class ClientController {
         return clientService.findAllClients();
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Client findClientById(@PathVariable Long id) {
+        return clientService.findClientById(id);
+    }
+
+    @GetMapping("/cpf/{cpf}")
+    @ResponseStatus(HttpStatus.OK)
+    public Client findClientByCpf(@PathVariable String cpf) {
+        return clientService.findClientByCpf(cpf);
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Client createClient(@RequestBody Client client) {
         return clientService.createClient(client);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Client updateClient(@RequestBody Client updatedClient, @PathVariable Long id) {
+        return clientService.updateClient(updatedClient, id);
     }
 
     @DeleteMapping("/{id}")
@@ -44,5 +62,4 @@ public class ClientController {
     public void deleteClient(@PathVariable Long id) {
         clientService.deleteClient(id);
     }
-    
 }
--- src/main/java/com/bankapi/bank/services/ClientService.java
+++ src/main/java/com/bankapi/bank/services/ClientService.java
@@ -23,17 +23,65 @@ public class ClientService {
         return clientRepository.findAll();
     }
 
+    public Client findClientById(Long id) {
+        Optional<Client> clientById = clientRepository.findById(id);
+
+        if(!clientById.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return clientRepository.findById(id).get();
+    }
+
+    public Client findClientByCpf(String cpf) {
+        Client clientByCpf = clientRepository.findByCpf(cpf);
+
+        if(clientByCpf == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return clientRepository.findByCpf(cpf);
+    }
+
     public Client createClient(Client client) {
-        Client clientByName = clientRepository.findByName(client.getName());
         Client clientByCpf = clientRepository.findByCpf(client.getCpf());
         Client clientByEmail = clientRepository.findByEmail(client.getEmail());
         Client clientByPhone = clientRepository.findByPhone(client.getPhone());
 
-        if(clientByName != null || clientByCpf != null || clientByEmail != null || clientByPhone != null) {
+        if(clientByCpf != null || clientByEmail != null || clientByPhone != null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
         client.setCreationDate(LocalDate.now());
+        client.setLastUpdate(LocalDateTime.now());
+
+        return clientRepository.save(client);
+    }
+
+    public Client updateClient(Client updatedClient, Long id) {
+        Optional<Client> clientDatabase = clientRepository.findById(id);
+        Client clientByCpf = clientRepository.findByCpf(updatedClient.getCpf());
+        Client clientByEmail = clientRepository.findByEmail(updatedClient.getEmail());
+        Client clientByPhone = clientRepository.findByPhone(updatedClient.getPhone());
+
+        if(!clientDatabase.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        else if(!updatedClient.getCpf().equals(clientDatabase.get().getCpf()) && clientByCpf != null ||
+                !updatedClient.getEmail().equals(clientDatabase.get().getEmail()) && clientByEmail != null 
+                || !updatedClient.getPhone().equals(clientDatabase.get().getPhone()) && clientByPhone != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        Client client = clientDatabase.get();
+
+        client.setName(updatedClient.getName());
+        client.setEmail(updatedClient.getEmail());
+        client.setCpf(updatedClient.getCpf());
+        client.setPhone(updatedClient.getPhone());
+        client.setAddress(updatedClient.getAddress());
+
+        client.setCreationDate(clientDatabase.get().getCreationDate());
         client.setLastUpdate(LocalDateTime.now());
 
         return clientRepository.save(client);
