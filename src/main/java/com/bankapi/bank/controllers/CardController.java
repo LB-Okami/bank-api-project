80e986758c0e5228d0a98b3e41e47d8384021707
LB-Okami <lucabtei@outlook.com>
2023-08-21 13:35:13 -0300

    Post & Get Card methods

--- src/main/java/com/bankapi/bank/controllers/CardController.java
+++ src/main/java/com/bankapi/bank/controllers/CardController.java
@@ -1,10 +1,19 @@
 package com.bankapi.bank.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bankapi.bank.model.Card;
+import com.bankapi.bank.model.CardDTO;
 import com.bankapi.bank.services.CardService;
 
 @RestController
@@ -13,4 +22,16 @@ import com.bankapi.bank.services.CardService;
 public class CardController {
     @Autowired
     private CardService cardService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Card> findAllCards() {
+        return cardService.findAllCards();
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Card createCard(@RequestBody CardDTO cardDTO) {
+        return cardService.createCard(cardDTO);
+    }
 }
--- src/main/java/com/bankapi/bank/services/CardService.java
+++ src/main/java/com/bankapi/bank/services/CardService.java
@@ -1,12 +1,59 @@
 package com.bankapi.bank.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.bankapi.bank.model.Account;
+import com.bankapi.bank.model.Card;
+import com.bankapi.bank.model.CardDTO;
 import com.bankapi.bank.repositories.CardRepository;
 
 @Service
 public class CardService {
     @Autowired
     private CardRepository cardRepository;
+
+    @Autowired
+    private AccountService accountService;
+
+    public List<Card> findAllCards() {
+        return cardRepository.findAll();
+    }
+
+    public Card createCard(CardDTO cardDTO) {
+        Card card = new Card();
+
+        Account accountById = accountService.findAccountById(cardDTO.getAccountId());
+
+        checkIfAccountHasCard(cardDTO.getAccountId());
+        
+        card.setName(cardDTO.getName());
+        card.setBrand(cardDTO.getBrand());
+        card.setLevel(cardDTO.getLevel());
+        card.setBill(cardDTO.getBill());
+        card.setCardNumber(cardDTO.getCardNumber());
+        card.setExpireDate(cardDTO.getExpireDate());
+        card.setCvv(cardDTO.getCvv());
+        card.setAccount(accountById);
+        
+        card.setCreationDate(LocalDate.now());
+        card.setLastUpdate(LocalDateTime.now());
+
+        return cardRepository.save(card);
+    }
+
+    public void checkIfAccountHasCard(Long id) {
+        Account accountById = accountService.findAccountById(id);
+
+        if(accountById.getCard() != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
