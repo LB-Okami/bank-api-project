a51b0de6d8604ab438688ed32ac5e400fc482af7
LB-Okami <lucabtei@outlook.com>
2023-09-05 15:16:43 -0300

    Spring Boot validations (Report)

--- src/main/java/com/bankapi/bank/model/Report.java
+++ src/main/java/com/bankapi/bank/model/Report.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -29,6 +30,7 @@ public class Report {
     private Operation operation;
 
     @NotNull
+    @Min(0)
     private Double value;
 
     private LocalDate creationDate;
--- src/main/java/com/bankapi/bank/model/ReportDTO.java
+++ src/main/java/com/bankapi/bank/model/ReportDTO.java
@@ -2,13 +2,19 @@ package com.bankapi.bank.model;
 
 import com.bankapi.bank.enums.Operation;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ReportDTO {
+    @NotNull
     private Operation operation;
 
+    @NotNull
+    @Min(0)
     private Double value;
 
+    @NotNull
     private Long cardId;
 }
